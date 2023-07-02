a8019d86860e4ef3406dae118777fe5b4e4e33be
element10101 <134858054+element10101@users.noreply.github.com>
2023-07-02 15:48:43 -0500

    Initialize everything

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from discord import Intents
+from discord.ext import commands
+from flask import Flask, render_template
+from replit import db
+from typing import Optional
+from os import environ
 
+intents = Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+
+app = Flask(__name__)
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+@app.route('/')
+def index():
+  return render_template("index.html"))
+
+app.run('0.0.0.0', port=81)
